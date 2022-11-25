9cd276117fb62e0d25b2f0dd8d865d714a24d310
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2022-11-25 15:26:40 +0100

    push script

--- Bash/git.sh
+++ Bash/git.sh
@@ -5,6 +5,7 @@ if [ "${#}" -eq 1 ]; then
     git add . 
     git commit -m "${1}"
     git push
+    git status 
 else 
     echo "Je moet een naam meegeven met het script deze wordt gebruikt als git commit message."
 fi
