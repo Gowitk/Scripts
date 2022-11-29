a82bc4bc5d61fa93e50497280d952a7ab289102b
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2022-11-29 13:26:02 +0100

    template

--- Bash/template.sh
+++ Bash/template.sh
@@ -2,6 +2,9 @@
 
 # Author: Jelle Van Holsbeeck <jelle.vanholsbeeck@outlook.com>
 
+# Abort on nonzero exitstatus
 set -o errexit
-set -o nounset 
+# Abort on unbound variable 
+set -o nounset
+# Don't hide errors within pipes
 set -o pipefail
