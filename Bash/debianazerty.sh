3545a6b80cbf050071bd23753c46d0ea3ed4dbee
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2023-04-20 01:51:34 +0200

    Delete debianazerty.sh

--- Bash/debianazerty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "XKBMODEL=\"pc105\"" | sudo tee -a /etc/default/keyboard > /dev/null
-echo "XKBLAYOUT=\"be\"" | sudo tee -a /etc/default/keyboard > /dev/null
-echo "XKBVARIANT=\"\"" | sudo tee -a /etc/default/keyboard > /dev/null
-echo "XKBOPTIONS=\"\"" | sudo tee -a /etc/default/keyboard > /dev/null
-
-# Reload keyboard configuration
-sudo dpkg-reconfigure -u keyboard-configuration
