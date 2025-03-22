3d5293f6152aa8d46710e51d24548d323aa76631
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2025-03-22 21:52:51 +0100

    install kalibox script

---
+++ Bash/fresh-kali.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Abort on nonzero exitstatus
+set -o errexit
+# Abort on unbound variable 
+set -o nounset
+# Don't hide errors within pipes
+set -o pipefail
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# Add a choice menu
+sudo apt install -y kali-linux-everything
+sudo apt install kali-wallpapers-2020.4
+
+sudo apt install -y flameshot
